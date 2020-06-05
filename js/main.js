e3299ef79eada8e50e620d2022f1711671147c32
Hakan Bamov <bamovh@icloud.com>
2020-06-05 11:17:37 +0300

    Fix small issues and refactor

--- js/main.js
+++ js/main.js
@@ -1,68 +1,68 @@
-// Initialize DataSource for messages
-const messageDataSource = new kendo.data.DataSource({
-    transport: {
-        read: (options) => {
-            options.success(messages);
+$(document).ready(function () {
+    // Initialize DataSource for Messages
+    const messageDataSource = new kendo.data.DataSource({
+        transport: {
+            read: (options) => {
+                options.success(messages);
+            },
+            schema: {
+                model: {
+                    fields: {
+                        category: { type: "string" },
+                        date: { type: "date" },
+                        fromEmail: { type: "string" },
+                        fromName: { type: "string" },
+                        html: { type: "string" },
+                        id: { type: "number" },
+                        isRead: { type: "number" },
+                        subject: { type: "string" },
+                        text: { type: "string" },
+                        toEmail: { type: "string" },
+                        toName: { type: "string" },
+                    }
+                }
+            }
+        },
+        pageSize: 10
+    });
+
+    // Initialize DataSource for Tasks
+    const tasksDataSource = new kendo.data.SchedulerDataSource({
+        transport: {
+            read: (options) => {
+                options.success(tasks);
+            },
         },
         schema: {
             model: {
+                id: "id",
                 fields: {
-                    category: { type: "string" },
-                    date: { type: "date" },
-                    fromEmail: { type: "string" },
-                    fromName: { type: "string" },
-                    html: { type: "string" },
-                    id: { type: "number" },
-                    isRead: { type: "number" },
-                    subject: { type: "string" },
-                    text: { type: "string" },
-                    toEmail: { type: "string" },
-                    toName: { type: "string" },
+                    taskId: { from: "id", type: "number" },
+                    title: { from: "title", defaultValue: "No title", validation: { required: true } },
+                    start: { type: "date", from: "starttime" },
+                    end: { type: "date", from: "endtime" },
+                    // startTimezone: { from: "starttimezone" },
+                    // endTimezone: { from: "endtimezone" },
+                    description: { from: "description" },
+                    recurrenceId: { from: "recurrenceid" },
+                    recurrenceRule: { from: "recurrencerule" },
+                    recurrenceException: { from: "recurrenceexception" },
+                    ownerId: { from: "ownerid", defaultValue: 1 },
+                    isAllDay: { type: "boolean", from: "isallday" }
                 }
             }
         }
-    },
-    pageSize: 10
-});
+    });
 
-// Initialize DataSource for tasks
-const tasksDataSource = new kendo.data.SchedulerDataSource({
-    transport: {
-        read: (options) => {
-            options.success(tasks);
-        },
-    },
-    schema: {
-        model: {
-            id: "id",
-            fields: {
-                taskId: { from: "id", type: "number" },
-                title: { from: "title", defaultValue: "No title", validation: { required: true } },
-                start: { type: "date", from: "starttime" },
-                end: { type: "date", from: "endtime" },
-                // startTimezone: { from: "starttimezone" },
-                // endTimezone: { from: "endtimezone" },
-                description: { from: "description" },
-                recurrenceId: { from: "recurrenceid" },
-                recurrenceRule: { from: "recurrencerule" },
-                recurrenceException: { from: "recurrenceexception" },
-                ownerId: { from: "ownerid", defaultValue: 1 },
-                isAllDay: { type: "boolean", from: "isallday" }
-            }
+    // Initialize DataSource for Contacts
+    const contactsDataSource = new kendo.data.DataSource({
+        transport: {
+            read: (options) => {
+                options.success(contacts);
+            },
         }
-    }
-});
+    });
 
-// Initialize DataSource for contacts
-const contactsDataSource = new kendo.data.DataSource({
-    transport: {
-        read: (options) => {
-            options.success(contacts);
-        },
-    }
-});
-
-$(document).ready(function () {
     // Initialization of tabs - Email, Calendar and Contacts
     const tabStrip = $("#tabstrip").kendoTabStrip({
         animation:  {
@@ -73,6 +73,10 @@ $(document).ready(function () {
         select: (event) => {
             const filteredCategory = event.item.getAttribute('section');
 
+            /*
+            Switch and the whole select event is needed to load Calendar
+            and Contacts tab only on first click of tabs
+            */
             switch(filteredCategory) {
                 case 'calendar':
                     if(typeof $("#scheduler").data('kendoScheduler') === "undefined") {
@@ -89,7 +93,7 @@ $(document).ready(function () {
         }
     });
 
-    // Initializationo of Menu Widget - Inbox, Important, Perso and Outbox
+    // Initialization of Menu Widget - Inbox, Important, Perso and Outbox
     const menu = $("#menu").kendoMenu({
         orientation: 'horizontal',
         select: (event) => {
@@ -120,7 +124,7 @@ $(document).ready(function () {
         columns: [
             {
                 field: "fromEmail",
-                title: "Sender",
+                title: "From",
                 width: "20%"
             },
             {
@@ -151,8 +155,10 @@ $(document).ready(function () {
         ],
         editable: "inline",
         change: (options) => {
-            // On click of grid row display email message under the grid
-            // and mark it as read
+            /*
+            On click of grid row display email message under the grid
+            and mark it as read
+            */
             var selectedRow = options.sender.select();
 
             if(selectedRow.length > 0) {
@@ -160,23 +166,42 @@ $(document).ready(function () {
 
                 rowData.set('isRead', 1);
 
-                var template = kendo.template("<div id='box'>#= html #</div>");
+                /*
+                Render email as a pretty Kendo UI Card
+                Reply and Forward buttons are only dummy buttons
+                */
+                var template = kendo.template(
+                    "<div class=\"k-card\">\n" +
+                        "<div class=\"k-card-header\">\n" +
+                            "<h5 class=\"k-card-title\">#= fromEmail #</h5>\n" +
+                            "<h6 class=\"k-card-subtitle\">#= kendo.toString(kendo.parseDate(date), \"MMMM dd, yyyy\") #</h6>\n" +
+                        "</div>\n" +
+                        "<div class=\"k-card-body\">\n" +
+                            "<p>#= html #</p>\n" +
+                        "</div>\n" +
+                        "<div class=\"k-card-actions k-card-actions-stretched\">\n" +
+                            "<span class=\"k-card-action\"><span class=\"k-button k-flat k-primary\">Reply</span></span>\n" +
+                            "<span class=\"k-card-action\"><span class=\"k-button k-flat k-primary\">Forward</span></span>\n" +
+                        "</div>\n" +
+                    "</div>"
+                )
+
                 var result = template(rowData);
                 $("#message").html(result);
             }
         }
     });
 
-    // Resize grid because of issue with tabs
+    // Resize grid to prevent tabs issue
     tabStrip.one("activate", () => {
         grid.resize();
     });
 
-    // Tab loading methods sharing one namespace
+    // Tab loading methods share one namespace
     const loadTabs = {
         calendar: () => {
             $("#scheduler").kendoScheduler({
-                height: 500,
+                height: 510,
                 views: [
                     "day",
                     "week",
